7d6bd0b35c1ef72a9285a2be1ef355f1d04a4f0b
shagunagra <shagunagrawal1998@gmail.com>
2020-05-05 14:40:39 -0700

    This is the third commit for the modals.

--- common.js
+++ common.js
@@ -1,19 +1,20 @@
-function cross(){
-    document.getElementById('id01').style.display='none';
-}
+var modalA = document.getElementById('id01');
 
+var modal = document.getElementById('id02');
 
-function openup(){
-    document.getElementById('id01').style.display='block';
-}
-
-
-var modal = document.getElementById('id01');
+var modalB = document.getElementById('id03');
 
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
-    }
+    } 
+  if(event.target == modalA) {
+        modalA.style.display = "none";      
+     }
+  if(event.target == modalB) {
+        modalA.style.display = "none";      
+     }
 }
+
 
 
