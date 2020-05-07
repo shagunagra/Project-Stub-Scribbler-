d43e1d052d8a8b8dddc2a3d776a67c84ee1fb238
shagunagra <59168880+shagunagra@users.noreply.github.com>
2020-05-07 08:44:46 -0600

    In this commit jquery for the signIn and signUp button is added.

--- Scripts/postslist.js
+++ Scripts/postslist.js
@@ -1,3 +1,16 @@
-function myfun(){
-    document.getElementById('del').style.display='block';
+/*function openbutton() {
+  document.getElementById('get_modal').style.display='block';
+  }*/
+
+var modalA = document.getElementById('id01');
+
+var modal = document.getElementById('id02');
+
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    } 
+  if(event.target == modalA) {
+        modalA.style.display = "none";      
+     }
 }
